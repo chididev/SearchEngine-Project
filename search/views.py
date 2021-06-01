d13a2fe80cff95b2448ce0d1218c9f2d93f5321b
Chidiebere Enekwe <chidieberehenry31@gmail.com>
2021-06-01 03:49:13 +0100

    made some modifications on the views

--- search/views.py
+++ search/views.py
@@ -36,33 +36,3 @@ def search(request):
 
     else:
         return render(request, 'search/search.html')
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def search(request):
-#     # if request.method == 'GET':
-#     #     word = request.GET.get('word')
-#     #     api = 'https://www.ask.com/web?q='+word
-#     #     result = request.get(api)
-#     #     soup = bs4(result.text, 'lxml')
-#     #     result_list = soup.find_all('div', {'class':'PartialSearchResults-item'})
-#     #     final_results = []
-#     #     for result in result_list:
-#     #         result_title = result.find(class_='PartialSearchResults-item-title').text
-#     #         result_url = result.find('a').get('href')
-#     #         result_desc = result.find(class_='PartialSearchResults-item-abstract').text
-#     #         final_results.append((result_title, result_url, result_desc))
-#     #     context = {
-#     #         'final_results':final_results,
-#     #     }
-#         return render(request, 'search/search.html')
